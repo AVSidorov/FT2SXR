c8df79ddd5ff4ffc800aa03538671b11b5a23bc5
Anton Sidorov <sidorov@mail.ioffe.ru>
2022-02-25 15:27:35 +0300

    Revert to console version

--- core/ft2_test.py
+++ core/ft2_test.py
@@ -2,8 +2,7 @@ import sys
 import signal
 from PyQt5 import QtWidgets
 
-# from console import Console
-from ADC.ADCUI import ADCUI_widget
+from console import Console
 from logger import Logger
 from core import Core
 
@@ -14,19 +13,16 @@ def main():
 
     core = Core(app)
 
-    ex = ADCUI_widget()
-    ex.channel0.connect(core.channel0)
-   # console = Console(app)
-   # console.channel0.connect(core.channel0)
+    console = Console(app)
+    console.channel0.connect(core.channel0)
 
     logger = Logger('log.txt', app)
     core.channel0.connect(logger.channel0_slot)
 
 
-    # echo = Logger(sys.stdout, app)
-    # core.channel0.connect(echo.channel0_slot)
+    echo = Logger(sys.stdout, app)
+    core.channel0.connect(echo.channel0_slot)
 
-    # core.channel0.connect(console.channel0_slot)
-    # console.run()
-    ex.show()
-    sys.exit(app.exec_())
+    core.channel0.connect(console.channel0_slot)
+    console.run()
+    app.exec()
