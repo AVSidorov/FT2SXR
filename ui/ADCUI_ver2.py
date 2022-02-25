9d415fe0ca7513342a5eddaae49304b34a72be69
misha <misha.bucz@mail.ru>
2022-02-25 20:08:15 +0300

    ADC UI updated (ver2)

--- ui/ADCUI_ver2.py
+++ ui/ADCUI_ver2.py
@@ -1,17 +1,24 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
-from ADCUI_design_ver2 import UI_ADC_widget
+from ADCUI_design_ver2 import Ui_ADC_widget
 
 
-class ADCUI_widget (QtWidgets.QWidget, UI_ADC_widget):
+class ADCUI_widget (QtWidgets.QWidget, Ui_ADC_widget):
     # channel0 = QtCore.pyqtSignal(bytes)
 
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.ch_list = [1, 2, 3, 4, 5, 6, 7, 8]
         self.enable_ch_list = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.on_flag = 0
+        self.enable_all_flag = 0
         self.enable_ch()
+        self.bias_list = [0., 0., 0., 0., 0., 0., 0., 0.]
+        self.current_channel = 1
+        self.frec = 0
+        self.source = ''
+        self.delay = 0
+        self.interval = 0
 
         self.ch1_checkBox.clicked.connect(self.enable_ch)
         self.ch2_checkBox.clicked.connect(self.enable_ch)
@@ -21,6 +28,12 @@ class ADCUI_widget (QtWidgets.QWidget, UI_ADC_widget):
         self.ch6_checkBox.clicked.connect(self.enable_ch)
         self.ch7_checkBox.clicked.connect(self.enable_ch)
         self.ch8_checkBox.clicked.connect(self.enable_ch)
+        self.frec_spinBox.valueChanged.connect(self.setfrec)
+        self.source_comboBox.currentTextChanged.connect(self.setsource)
+        self.delay_spinBox.valueChanged.connect(self.setdelay)
+        self.interval_spinBox.valueChanged.connect(self.setinterval)
+        self.ch_comboBox.currentTextChanged.connect(self.showchannel)
+        self.bias_doubleSpinBox.valueChanged.connect(self.setbias)
 
     def enable_ch(self):
         self.enable_ch_list[0] = bool(self.ch1_checkBox.checkState())
@@ -32,23 +45,45 @@ class ADCUI_widget (QtWidgets.QWidget, UI_ADC_widget):
         self.enable_ch_list[6] = bool(self.ch7_checkBox.checkState())
         self.enable_ch_list[7] = bool(self.ch8_checkBox.checkState())
 
-        self.bias1_doubleSpinBox.setEnabled(self.enable_ch_list[0])
-        self.bias2_doubleSpinBox.setEnabled(self.enable_ch_list[1])
-        self.bias3_doubleSpinBox.setEnabled(self.enable_ch_list[2])
-        self.bias4_doubleSpinBox.setEnabled(self.enable_ch_list[3])
-        self.bias5_doubleSpinBox.setEnabled(self.enable_ch_list[4])
-        self.bias6_doubleSpinBox.setEnabled(self.enable_ch_list[5])
-        self.bias7_doubleSpinBox.setEnabled(self.enable_ch_list[6])
-        self.bias8_doubleSpinBox.setEnabled(self.enable_ch_list[7])
+        self.ch_list = []
+        for i in range(len(self.enable_ch_list)):
+            if self.enable_ch_list[i]:
+                self.ch_list.append(i+1)
+        self.ch_comboBox.clear()
+        for i in range(len(self.ch_list)):
+            self.ch_comboBox.addItem(str(self.ch_list[i]))
 
-        self.on_flag = False if sum(self.enable_ch_list) == 0 else True
+        self.enable_all_flag = False if sum(self.enable_ch_list) == 0 else True
 
-        self.frec_spinBox.setEnabled(self.on_flag)
-        self.source_comboBox.setEnabled(self.on_flag)
-        self.delay_spinBox.setEnabled(self.on_flag)
-        self.interval_spinBox.setEnabled(self.on_flag)
+        self.frec_spinBox.setEnabled(self.enable_all_flag)
+        self.source_comboBox.setEnabled(self.enable_all_flag)
+        self.delay_spinBox.setEnabled(self.enable_all_flag)
+        self.interval_spinBox.setEnabled(self.enable_all_flag)
+        self.bias_doubleSpinBox.setEnabled(self.enable_all_flag)
+        self.ch_comboBox.setEnabled(self.enable_all_flag)
 
-        # self.channel0.emit(b'btnclicked')
+        # self.channel0.emit
+    def setfrec(self):
+        self.frec = int(self.frec_spinBox.value())
+
+    def setsource(self):
+        self.source = self.source_comboBox.currentText()
+
+    def setdelay(self):
+        self.delay = self.delay_spinBox.value()
+        print(self.delay)
+
+    def setinterval(self):
+        self.interval = self.interval_spinBox.value()
+        print(self.interval)
+
+    def showchannel(self):
+        if self.ch_comboBox.currentText() != '':
+            self.current_channel = int(self.ch_comboBox.currentText())
+        self.bias_doubleSpinBox.setValue(self.bias_list[self.current_channel-1])
+
+    def setbias(self):
+        self.bias_list[self.current_channel-1] = self.bias_doubleSpinBox.value()
 
 
 def main():
