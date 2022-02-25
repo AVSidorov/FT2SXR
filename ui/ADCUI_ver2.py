c2ff82fa8845e144485193a30304f0cdc8f4e22e
misha <misha.bucz@mail.ru>
2022-02-25 21:16:11 +0300

    ADC UI updated (*_ver2.py)

--- ui/ADCUI_ver2.py
+++ ui/ADCUI_ver2.py
@@ -9,17 +9,23 @@ class ADCUI_widget (QtWidgets.QWidget, Ui_ADC_widget):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.ch_list = [1, 2, 3, 4, 5, 6, 7, 8]
-        self.enable_ch_list = [0, 0, 0, 0, 0, 0, 0, 0]
-        self.enable_all_flag = 0
-        self.enable_ch()
-        self.bias_list = [0., 0., 0., 0., 0., 0., 0., 0.]
-        self.current_channel = 1
-        self.frec = 0
+
+        # ADC values
+        self.channels_status = [0, 0, 0, 0, 0, 0, 0, 0]  # 0/1 or True/False
+        self.biases = [0., 0., 0., 0., 0., 0., 0., 0.]
+        self.frequency = 0
         self.source = ''
         self.delay = 0
         self.interval = 0
 
+        # service data
+        self.active_channels = [1, 2, 3, 4, 5, 6, 7, 8]
+        self.enable_all_channels_flag = 0
+        self.current_channel = 1
+
+        self.enable_ch()
+
+        # signals
         self.ch1_checkBox.clicked.connect(self.enable_ch)
         self.ch2_checkBox.clicked.connect(self.enable_ch)
         self.ch3_checkBox.clicked.connect(self.enable_ch)
@@ -28,7 +34,7 @@ class ADCUI_widget (QtWidgets.QWidget, Ui_ADC_widget):
         self.ch6_checkBox.clicked.connect(self.enable_ch)
         self.ch7_checkBox.clicked.connect(self.enable_ch)
         self.ch8_checkBox.clicked.connect(self.enable_ch)
-        self.frec_spinBox.valueChanged.connect(self.setfrec)
+        self.frec_spinBox.valueChanged.connect(self.setfreq)
         self.source_comboBox.currentTextChanged.connect(self.setsource)
         self.delay_spinBox.valueChanged.connect(self.setdelay)
         self.interval_spinBox.valueChanged.connect(self.setinterval)
@@ -36,35 +42,38 @@ class ADCUI_widget (QtWidgets.QWidget, Ui_ADC_widget):
         self.bias_doubleSpinBox.valueChanged.connect(self.setbias)
 
     def enable_ch(self):
-        self.enable_ch_list[0] = bool(self.ch1_checkBox.checkState())
-        self.enable_ch_list[1] = bool(self.ch2_checkBox.checkState())
-        self.enable_ch_list[2] = bool(self.ch3_checkBox.checkState())
-        self.enable_ch_list[3] = bool(self.ch4_checkBox.checkState())
-        self.enable_ch_list[4] = bool(self.ch5_checkBox.checkState())
-        self.enable_ch_list[5] = bool(self.ch6_checkBox.checkState())
-        self.enable_ch_list[6] = bool(self.ch7_checkBox.checkState())
-        self.enable_ch_list[7] = bool(self.ch8_checkBox.checkState())
+        self.channels_status[0] = bool(self.ch1_checkBox.checkState())
+        self.channels_status[1] = bool(self.ch2_checkBox.checkState())
+        self.channels_status[2] = bool(self.ch3_checkBox.checkState())
+        self.channels_status[3] = bool(self.ch4_checkBox.checkState())
+        self.channels_status[4] = bool(self.ch5_checkBox.checkState())
+        self.channels_status[5] = bool(self.ch6_checkBox.checkState())
+        self.channels_status[6] = bool(self.ch7_checkBox.checkState())
+        self.channels_status[7] = bool(self.ch8_checkBox.checkState())
 
-        self.ch_list = []
-        for i in range(len(self.enable_ch_list)):
-            if self.enable_ch_list[i]:
-                self.ch_list.append(i+1)
+        # make drop-down list
+        self.active_channels = []
+        for i in range(len(self.channels_status)):
+            if self.channels_status[i]:
+                self.active_channels.append(i + 1)
         self.ch_comboBox.clear()
-        for i in range(len(self.ch_list)):
-            self.ch_comboBox.addItem(str(self.ch_list[i]))
+        for i in range(len(self.active_channels)):
+            self.ch_comboBox.addItem(str(self.active_channels[i]))
 
-        self.enable_all_flag = False if sum(self.enable_ch_list) == 0 else True
+        # disable all fields
+        self.enable_all_channels_flag = False if sum(self.channels_status) == 0 else True
 
-        self.frec_spinBox.setEnabled(self.enable_all_flag)
-        self.source_comboBox.setEnabled(self.enable_all_flag)
-        self.delay_spinBox.setEnabled(self.enable_all_flag)
-        self.interval_spinBox.setEnabled(self.enable_all_flag)
-        self.bias_doubleSpinBox.setEnabled(self.enable_all_flag)
-        self.ch_comboBox.setEnabled(self.enable_all_flag)
+        self.frec_spinBox.setEnabled(self.enable_all_channels_flag)
+        self.source_comboBox.setEnabled(self.enable_all_channels_flag)
+        self.delay_spinBox.setEnabled(self.enable_all_channels_flag)
+        self.interval_spinBox.setEnabled(self.enable_all_channels_flag)
+        self.bias_doubleSpinBox.setEnabled(self.enable_all_channels_flag)
+        self.ch_comboBox.setEnabled(self.enable_all_channels_flag)
 
         # self.channel0.emit
-    def setfrec(self):
-        self.frec = int(self.frec_spinBox.value())
+
+    def setfreq(self):
+        self.frequency = int(self.frec_spinBox.value())
 
     def setsource(self):
         self.source = self.source_comboBox.currentText()
@@ -80,10 +89,10 @@ class ADCUI_widget (QtWidgets.QWidget, Ui_ADC_widget):
     def showchannel(self):
         if self.ch_comboBox.currentText() != '':
             self.current_channel = int(self.ch_comboBox.currentText())
-        self.bias_doubleSpinBox.setValue(self.bias_list[self.current_channel-1])
+        self.bias_doubleSpinBox.setValue(self.biases[self.current_channel - 1])
 
     def setbias(self):
-        self.bias_list[self.current_channel-1] = self.bias_doubleSpinBox.value()
+        self.biases[self.current_channel - 1] = self.bias_doubleSpinBox.value()
 
 
 def main():
