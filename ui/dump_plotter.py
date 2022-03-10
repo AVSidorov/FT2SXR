3cc0d21603580bc19cd66bf65955bb5c4b2c787b
Anton Sidorov <sidorov@mail.ioffe.ru>
2022-03-10 18:51:02 +0300

    dump_plotter.py plots adc data from dump file

---
+++ ui/dump_plotter.py
@@ -0,0 +1,54 @@
+from core.core import Core
+from core.sxr_protocol_pb2 import MainPacket, AdcStatus
+import matplotlib.pyplot as plt
+import os
+import datetime
+import numpy as np
+
+
+class DumpPlotter(Core):
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.address = 12
+        self._status = None
+
+        td = datetime.date.today()
+        wdir = format(td.year - 2000, '02d') + format(td.month, '02d') + format(td.day, '02d')
+        self.dump_file = os.path.join(os.path.join(os.path.abspath('./'), wdir), 'data_0.bin')
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, status):
+        if isinstance(status, bytes):
+            self._status = AdcStatus()
+            self._status.ParseFromString(status)
+        elif isinstance(status, AdcStatus):
+            self._status = status
+
+    def channel0_slot(self, data: bytes):
+        request = MainPacket()
+        request.ParseFromString(data)
+        if request.sender == 1:
+            if request.command in (0, 1):
+                self.status.ParseFromString(request.data)
+            if request.command == 2:
+
+                n_ch = 0
+                if len(self.status.board_status) > 0:
+                    if len(self.status.board_status[0].channel_mask) > 0:
+                        mask = int().from_bytes(self.status.board_status[0].channel_mask, 'big')
+                        n_ch = bin(mask).count("1")
+                    elif len(self.status.board_status[0].channel_status) > 0:
+                        n_ch = sum([ch.enabled for ch in self.status.board_status[0].channel_status])
+                if n_ch > 0 and os.path.exists(self.dump_file):
+                    dump = np.fromfile(self.dump_file, dtype=np.int16)
+                    dump = dump.reshape((-1, n_ch)).T
+                    cols = (_ for _ in dump)
+
+                    for col, row in zip(cols, range(1, n_ch+1)):
+                        plt.subplot(n_ch, 1, row)
+                        plt.plot(col)
+                    plt.show()
