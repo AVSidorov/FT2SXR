dbac5f7ddb851cf94c13bbb91aaf42e32184b555
misha <misha.bucz@mail.ru>
2022-03-17 03:01:11 +0300

    useless files removed

--- ui/CentralWidgetUI.py
+++
@@ -1,9 +0,0 @@
-from PyQt5 import QtWidgets
-from ui.CentralWidgetUIDesign import Ui_MainWidgetDesign
-
-
-class MainWidget(QtWidgets.QWidget, Ui_MainWidgetDesign):
-    def __init__(self, parent=None):
-        super().__init__(parent=parent)
-        self.setupUi(self)
-        MainWidget.setFixedSize(self, 553, 462)
