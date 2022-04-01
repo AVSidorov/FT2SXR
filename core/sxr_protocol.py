edcdd182217aecc4c9a1e789d995006cb098ca4b
Anton Sidorov <sidorov@mail.ioffe.ru>
2022-04-01 13:23:48 +0300

    sxr_protocol added testing for input data.

--- core/sxr_protocol.py
+++ core/sxr_protocol.py
@@ -1,5 +1,5 @@
 from core.sxr_protocol_pb2 import MainPacket
-
+import warnings
 
 def packet_init(address, sender):
     request = MainPacket()
@@ -7,3 +7,13 @@ def packet_init(address, sender):
     request.sender = sender
     return request
 
+
+def isPacketOfType(data, packetT):
+    pkt = packetT()
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter("always")
+        pkt.ParseFromString(data)
+    if len(w) == 0:
+        return True
+    else:
+        return False
