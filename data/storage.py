c135fe152a2bd45a238b39a7f2aeda053c82d32a
Anton Sidorov <sidorov@mail.ioffe.ru>
2022-04-21 17:02:54 +0300

    storage as tree. InSys storage as special type of storage

--- data/storage.py
+++ data/storage.py
@@ -1,61 +1,127 @@
+import os.path
+
 import paramiko
 from scp import SCPClient
 import posixpath as ospath
-from core.fileutils import today_dir, work_dir
 import time
+from data.filenode import FileNode
+import pytest
 
 
-class Storage:
-    def __init__(self, path='/mnt/nvme0n1p1', wdir=None, protocol='scp', **kwargs):
+class Storage(FileNode):
+    def __init__(self):
+        super().__init__()
+        self.storage = self
+        self.nodes = list()
+        self.nodes.append(self)
+
+    def add(self, path):
+        cur = self
+        for dir_name in path.strip('/').rstrip('/').split('/'):
+            if dir_name not in cur:
+                cur[dir_name] = FileNode(name=dir_name, parent=cur)
+                self.nodes.append(cur[dir_name])
+            cur = cur[dir_name]
+
+    def __call__(self, path):
+        current = self
+        for dir_name in path.strip('/').rstrip('/').split('/'):
+            if dir_name in current:
+                current = current[dir_name]
+            else:
+                return None
+        return current
+
+    def visit(self, func):
+        for node in self.all_in_deep():
+            func(node.name)
+
+    def all_in_deep(self, start=None):
+        if start is None:
+            start = self
+        else:
+            start = self(start)
+        visited = set()
+        self.nodes = list()
+        stack = list()
+        stack.append(start)
+        while len(stack) > 0:
+            cur = stack[-1]
+            if cur not in visited:
+                visited.add(cur)
+                self.nodes.append(cur)
+                stack.extend([cur[_] for _ in reversed(cur)])
+            else:
+                stack.pop(-1)
+
+    def all_in_width(self, start=None):
+        if start is None:
+            start = self
+        else:
+            start = self(start)
+        visited = set()
+        self.nodes = list()
+        stack = list()
+        stack.append(start)
+        while len(stack) > 0:
+            cur = stack[0]
+            if cur not in visited:
+                visited.add(cur)
+                self.nodes.append(cur)
+                stack.extend([cur[_] for _ in cur])
+            else:
+                stack.pop(0)
+
+
+class InSysStorage(Storage):
+    def __init__(self):
+        super().__init__()
+        self.name = '/InSysADC'
         self.params = dict()
         self.params['hostname'] = '192.168.0.242'
         self.params['username'] = "adc_user"
         self.params['password'] = "adc_user"
         self.params['look_for_keys'] = False
         self.params['allow_agent'] = False
-        self.path = path
-        self.work_dir = today_dir(wdir)
-        self.protocol = protocol
+        self.path = '/mnt/nvme0n1p1'
 
-        if len(kwargs) != 0:
-            self.params = kwargs
+    def abspath_internal(self, path: str = ''):
+        path = './' + path
+        return ospath.normpath(ospath.join(self.path, path))
 
-    def get(self, filename, path=None):
-        if filename[0] == '/':
-            filename = '.'+filename
+    def get(self, filename: str, path: str = None):
+        filename = self.abspath_internal(filename)
 
         if path is None:
-            path = self.work_dir
+            path = os.path.abspath('./')
 
-        if self.protocol == 'scp':
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            try:
-                client.connect(**self.params)
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(**self.params)
+            scp = SCPClient(client.get_transport())
+            scp.get(ospath.join(self.path, filename), path)
+        except:
+            pass
+        finally:
+            client.close()
+
+    def put(self, filename: str, path: str):
+        path = self.abspath_internal(path)
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(**self.params)
+            _, stdout, stderr = client.exec_command(f'mkdir -p {path}')
+            time.sleep(1)
+            err = stderr.read()
+            if len(err) == 0:
                 scp = SCPClient(client.get_transport())
-                scp.get(ospath.join(self.path, filename), self.work_dir)
-            except:
-                pass
-            finally:
-                client.close()
-
-    def put(self, filename, path):
-        if path[0] == '/':
-            path = '.'+path
-        if self.protocol == 'scp':
-            client = paramiko.SSHClient()
-            client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            try:
-                client.connect(**self.params)
-                _, stdout, stderr = client.exec_command(f'mkdir -p {ospath.join(self.path, ospath.split(path)[0])}\n')
-                time.sleep(1)
-                if len(stderr.read()) == 0:
-                    scp = SCPClient(client.get_transport())
-                    scp.put(ospath.abspath(filename), ospath.join(self.path, path))
-            except:
-                pass
-            finally:
-                client.close()
+                scp.put(ospath.abspath(filename), path)
+        except:
+            pass
+        finally:
+            client.close()
 
     def get_tree(self):
         out = ''
@@ -74,28 +140,44 @@ class Storage:
             client.close()
 
         lines = out.split('\n')
-        tree = dict()
-        p = None
+        folder = None
         for line in lines:
             if all((len(line) != 0, line.find('total') != 0)):
                 if all((line[0] == '/', line[-1] == ':')):
-                    p = tree
-                    for dir_name in line.strip('/').rstrip(':').split('/'):
-                        if not dir_name in p:
-                            p[dir_name] = dict()
-                        p = p[dir_name]
-                elif p is not None:
+                    folder = '/'+line[len(self.path):].rstrip(':/').strip('/')
+                elif folder is not None:
                     fields = line.split()
-                    p[fields[8]] = dict()
-                    if fields[0][0] == '-':
-                        p[fields[8]]['mode'] = fields[0][1:]
-                        p[fields[8]]['user'] = fields[2]
-                        p[fields[8]]['group'] = fields[3]
-                        p[fields[8]]['size'] = fields[4]
-                        p[fields[8]]['date'] = fields[5:8]
-                    elif fields[0][0] == 'l':
-                        p[fields[8]] = fields[-1]
-        if len(tree) > 0:
-            for dir_name in self.path.strip('/').rstrip(':').split('/'):
-                tree = tree[dir_name]
-        return tree
+                    self.add(folder+'/'+fields[8])
+
+    def exist(self, path: str):
+        path = self.abspath_internal(path)
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        try:
+            client.connect(**self.params)
+            _, stdout, stderr = client.exec_command(f'ls -d {path}')
+            out = stdout.readline().replace('\n', '')
+            err = stderr.read()
+            if len(err) == 0 and ospath.normpath(out) == ospath.normpath(path):
+                return True
+            elif len(err) > 0:
+                return False
+            else:
+                return None
+
+        except:
+            pass
+        finally:
+            client.close()
+
+
+def test_abspath():
+    st = InSysStorage()
+    assert st.abspath_internal() == st.path
+    assert st.abspath_internal('') == st.path
+    assert st.abspath_internal('a') == st.path + '/'+'a'
+    assert st.abspath_internal('/a') == st.path + '/'+'a'
+    assert st.abspath_internal('./a') == st.path + '/'+'a'
+    assert st.abspath_internal('.a') == st.path + '/'+'.a'
+    assert st.abspath_internal('/.a') == st.path + '/'+'.a'
