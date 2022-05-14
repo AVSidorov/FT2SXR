57d2a8fb8e045ea0f515ef671bf887be4bef5ad7
misha <misha.bucz@mail.ru>
2022-05-15 02:41:52 +0300

    new amplifier window

--- ui/AmplifierUI.py
+++ ui/AmplifierUI.py
@@ -1,4 +1,6 @@
+import csv
 import sys
+import os
 from PyQt5 import QtWidgets
 from ui.AmplifierUIDesign import Ui_AmplifierWidgetDesign
 
@@ -13,12 +15,54 @@ class AmplifierWidget(QtWidgets.QWidget, Ui_AmplifierWidgetDesign):
         self.gainA = 0
         self.gainB = 0
         self.decay = 0
+        self.switch_state = 0b0000
 
         # signals
         self.gainA_doubleSpinBox.valueChanged.connect(self.setgainA)
         self.gainB_doubleSpinBox.valueChanged.connect(self.setgainB)
-        self.decay_doubleSpinBox.valueChanged.connect(self.setdecay)
+        self.time45_checkBox.stateChanged.connect(self.setdecay)
+        self.time9_checkBox.stateChanged.connect(self.setdecay)
+        self.time13_checkBox.stateChanged.connect(self.setdecay)
+        self.time17_checkBox.stateChanged.connect(self.setdecay)
 
+        cal_file = []
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'amp_cal.csv'), newline='') as file:
+            cal = csv.DictReader(file, delimiter=';')
+            for i in cal:
+                cal_file.append(i)
+        self.cal_textBrowser.setText(self.make_table(cal_file))
+
+        self.setdecay()
+
+    def make_table(self, cal_file):
+        table = '<!DOCTYPE HTML>' \
+                '<html>' \
+                '<head>' \
+                '<meta charset="utf-8">' \
+                '<title>Калибровка</title>' \
+                '</head>'
+
+        cal_file = sorted(cal_file, key=lambda d: f'{d["Channel"]}{d["Config"]}')
+
+        prev = {'Channel': '', 'Config': ''}
+        n = 10
+        for i in cal_file:
+            if i['Channel'] != prev['Channel'] or i['Config'] != prev['Config']:
+                table += '</table></body>'
+
+                table += '<body><table border="1" align="center" width="160">'
+                table += f'<caption>{i["Channel"]} ({i["Config"]})</caption>'
+                table += f'<tr>' \
+                         f'<th>{"Limb"}</th>' \
+                         f'<th>{"Amp"}</th>' \
+                         f'</tr>'
+                table += f'<tr><td>{i["Limb"]}</td><td>{i["Amp"]}</td></tr>'
+            elif i['Channel'] == prev['Channel'] and i['Config'] == prev['Config']:
+                table += f'<tr><td>{i["Limb"]}</td><td>{i["Amp"]}</td></tr>'
+            prev = i
+        table.replace('</table></body>', '', 1)
+        table += '</html>'
+        return table
 
     def setgainA(self):
         self.gainA = self.gainA_doubleSpinBox.value()
@@ -27,7 +71,42 @@ class AmplifierWidget(QtWidgets.QWidget, Ui_AmplifierWidgetDesign):
         self.gainB = self.gainB_doubleSpinBox.value()
 
     def setdecay(self):
-        self.decay = self.decay_doubleSpinBox.value()
+        self.time45_checkBox.setDisabled(True)
+        self.time9_checkBox.setDisabled(True)
+        self.time13_checkBox.setDisabled(True)
+        self.time17_checkBox.setDisabled(True)
+
+        c = 1e-8  # F
+        r0 = 30037.8  # Ohm (then all switches off)
+        r45 = 206.21  # Ohm 205
+        r9 = 415.27  # Ohm 412
+        r13 = 603.54  # Ohm 619
+        r17 = 794.16  # Ohm 825
+        t09 = 0.105361  # RC
+        t01 = 2.30259  # RC
+
+        new_state = 0b0000
+        if self.time45_checkBox.checkState():
+            new_state |= 0b0001
+        if self.time9_checkBox.checkState():
+            new_state |= 0b0010
+        if self.time13_checkBox.checkState():
+            new_state |= 0b0100
+        if self.time17_checkBox.checkState():
+            new_state |= 0b1000
+        self.switch_state = new_state
+
+        r = (1/r0 + int(new_state & 0b0001)/r45 + int(new_state >> 1 & 0b0001)/r9
+             + int(new_state >> 2 & 0b0001)/r13 + int(new_state >> 3 & 0b0001)/r17)**(-1)
+        t = (t01-t09) * c * r * 1e6  # ms
+        t = round(t, 1)
+        self.decay = t
+        self.sum_time_label.setText(str(t))
+
+        self.time45_checkBox.setEnabled(True)
+        self.time9_checkBox.setEnabled(True)
+        self.time13_checkBox.setEnabled(True)
+        self.time17_checkBox.setEnabled(True)
 
 
 def main():
