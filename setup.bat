813a7d6c3ca1c7de207265967e59315e05b557c7
misha <misha.bucz@mail.ru>
2023-02-04 21:34:53 +0300

    started to make setup file

---
+++ setup.bat
@@ -0,0 +1,9 @@
+pip install --upgrade pip
+pip install numpy
+pip install matplotlib
+pip install pyqt5
+pip install silx
+pip install h5py
+pip install paramiko
+pip install protobuf
+pip install scp
