79b96409a5dce78b20d553c658b04b9335efcc34
misha <misha.bucz@mail.ru>
2023-02-04 21:59:20 +0300

    new h5 file names

--- core/core.py
+++ core/core.py
@@ -135,6 +135,18 @@ class Dev(Core):
 
         if isinstance(request, str):
             file_origin = request
+            cur_dir = file_origin.split('/')[0]
+            # print(cur_dir)
+            cur_num = 0
+            list_dir = os.listdir(cur_dir)
+            while True:
+                file = f'{self.name}{curtime.year-2000:02d}{curtime.month:02d}{curtime.day:02d}_{str(cur_num).zfill(3)}.h5'
+                if file in list_dir:
+                    cur_num += 1
+                    continue
+                else:
+                    break
+            # print(file)
 
         if file_origin is not None:
             if len(file_origin) > 0:
